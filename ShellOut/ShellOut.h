00c91d5dba7d45760190e1c35230214e86b3aa1a
Thibaud Robelain <trob@yelp.com>
2019-04-08 18:00:11 -0700

    Add xcodeproj

---
+++ ShellOut/ShellOut.h
@@ -0,0 +1,19 @@
+//
+//  ShellOut.h
+//  ShellOut
+//
+//  Created by Thibaud Robelain on 4/8/19.
+//  Copyright © 2019 Thibaud Robelain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ShellOut.
+FOUNDATION_EXPORT double ShellOutVersionNumber;
+
+//! Project version string for ShellOut.
+FOUNDATION_EXPORT const unsigned char ShellOutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShellOut/PublicHeader.h>
+
+
